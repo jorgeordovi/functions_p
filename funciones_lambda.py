3057a3c11e6562759a4269a380b3c4565376f2fe
jorgeordovi <jorgeordovi@gmail.com>
2025-01-21 05:13:40 -0500

    Fuciones valor

---
+++ funciones_lambda.py
@@ -0,0 +1,7 @@
+def duplicar(num):
+    return num * 2
+#print(duplicar(5))
+
+duplicar_lambda = lambda num: num * 2
+
+print(duplicar_lambda(5))
---
+++ recursion_p2.py
@@ -0,0 +1,13 @@
+def suma(a,b):
+    """_summary_
+
+    Args:
+        a (int): first number 2 sum
+        b (int): second number 2 sum
+
+    Returns:
+        int: the sum of boh numbers
+    """
+    return a + b
+
+print(suma.__doc__)
